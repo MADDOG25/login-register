832555c6dbc3024ea9c96a0ac3820f71d84ad9e5
Dev Jeffrey <mdog1525@gmail.com>
2024-05-30 12:41:03 -0500

    LoginPage modified

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,12 @@
 
 export default function LoginPage() {
   return (
-    <div>LoginPage</div>
+    <div>
+      <form>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
   )
 }
