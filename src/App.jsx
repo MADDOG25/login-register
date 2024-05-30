aaaa7717ff31ae8f7af9f72f1fefe80c46eae962
Dev Jeffrey <mdog1525@gmail.com>
2024-05-30 12:33:19 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import HomePage from './pages/HomePage'
+export default function App() {
+  return (
+    <div className="bg-slate-700 h-screen">
+      <HomePage/>
+      
+    </div>
+  )
+}
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+
+export default function HomePage() {
+    return (
+      <div>
+        <h1 className="text-3xl text-center p-8">Home Page</h1>
+        
+      </div>
+    )
+  }
+  
