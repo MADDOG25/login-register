5be83a1678302c4143a992e3ef32829a4c54980d
Dev Jeffrey <mdog1525@gmail.com>
2024-05-30 12:45:37 -0500

    delete inputs form

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,9 +3,7 @@ export default function LoginPage() {
   return (
     <div>
       <form>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
+        
       </form>
     </div>
   )
